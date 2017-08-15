bffe37fb344cf883ab4b57318c6c2eb7cfef75a7
inkyung <dkwmzk1016@naver.com>
2017-08-15 17:29:03 +0900

    splash activity

---
+++ Amour/app/src/main/java/orgm/androidtown/amour/SplashActivity.java
@@ -0,0 +1,30 @@
+package orgm.androidtown.amour;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by InKyung on 2017-08-15.
+ */
+
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },3000);
+    }
+}
