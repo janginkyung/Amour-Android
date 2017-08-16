7f8060ad25e755d7cc366156723503bc33a3d335
inkyung <dkwmzk1016@naver.com>
2017-08-16 17:36:14 +0900

    signup UI

--- Amour/app/src/main/java/orgm/androidtown/amour/MainActivity.java
+++ Amour/app/src/main/java/orgm/androidtown/amour/MainActivity.java
@@ -1,8 +1,8 @@
 package orgm.androidtown.amour;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, SplashActivity.class));
+
     }
 }
--- Amour/app/src/main/java/orgm/androidtown/amour/SplashActivity.java
+++ Amour/app/src/main/java/orgm/androidtown/amour/SplashActivity.java
@@ -23,6 +23,9 @@ public class SplashActivity extends Activity{
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+
                 finish();
             }
         },3000);
